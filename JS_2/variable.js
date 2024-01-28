e60aa761e8284b243ac15ae51740a6270b48a253
ashwinlate46 <157834243+ashwinlate46@users.noreply.github.com>
2024-01-28 04:59:49 +0000

    javascript variables

---
+++ JS_2/variable.js
@@ -0,0 +1,28 @@
+//Variables
+
+var Firstname;  //Declaring Variable
+
+Firstname = "Ashwin"   //Assigning Value
+
+//Use a Variable
+console.log(Firstname);
+
+//Change Value
+
+Firstname = "AshwinLate";
+console.log(Firstname);
+
+//Number
+
+let x = 5;
+let y = 2;
+
+let z = x + y;
+
+console.log(z);
+
+let a = 10;
+const b = 2;
+
+a = 20;
+console.log(a+b)
